04c1c6e561f500778198edb26c77d144fa8be671
Michael Sarahan <msarahan@gmail.com>
2019-12-18 13:11:18 -0600

    simplify, fix overlinking warning, reset build number

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include $CFLAGS"
-
-if [[ $(uname) == Darwin ]]; then
-    export CC=clang
-    export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -headerpad_max_install_names $LDFLAGS"
-    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-    export MACOSX_DEPLOYMENT_TARGET="10.9"
-elif [ $(uname) == Linux ] ; then
-    export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-fi
-
 # remove libtool files
 find $PREFIX -name '*.la' -delete
 
 ./configure --prefix=$PREFIX \
 	    --disable-dependency-tracking \
 	    --disable-debug \
+	    --enable-shared \
+	    --enable-static \
 	    --enable-nasm
 
 make -j$CPU_COUNT
